e3d8955014912f562bcf004609077fd6d92509e3
hrachyamartirosyan858 <hrachyamartirosyan858@gmail.com>
2021-08-15 18:42:32 +0400

    workin on import func

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 let tetrisArr = [
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 1, 1, 0, 0, 0, 0, 0, 0, 0],
-    [0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -15,10 +12,13 @@ let tetrisArr = [
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 2],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 2],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 2],
-    [0, 0, 2, 0, 0, 0, 0, 0, 0, 2]
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 ];
 
 function isPieceGoingDownOk(arr) {
@@ -57,8 +57,6 @@ function pieceGoesDown(arr) {
     return arr;
 }
 
-// console.log(pieceGoesDown(tetrisArr));
-
 function isPieceGoingRightOk(arr) {
     for (let i = 0; i < 20; i++) {
         for (let j = 9; j >= 0; j--) {
@@ -85,9 +83,6 @@ function pieceGoesRight(arr) {
     }
     return arr;
 }
-
-
-// console.log(pieceGoesRight(tetrisArr));
 
 function isPieceGoingLeftOk(arr) {
     for (let i = 0; i < 20; i++) {
@@ -116,7 +111,44 @@ function pieceGoesLeft(arr) {
     return arr;
 }
 
-console.log(pieceGoesLeft(tetrisArr));
+const button = document.querySelector(".button");
+
+function renderItems(arr) {
+    document.querySelector(".tetris-field").innerHTML = "";
+    for (let i = 0; i < 20; i++) {
+        for (let j = 0; j < 10; j++) {
+            if (arr[i][j] === 0) {
+                const div = document.createElement("div");
+                div.classList.add("white");
+                document.querySelector(".tetris-field").append(div);
+            } else if (arr[i][j] === 1) {
+                const div = document.createElement("div");
+                div.classList.add("red");
+                document.querySelector(".tetris-field").append(div);
+            } else if (arr[i][j] === 2) {
+                const div = document.createElement("div");
+                div.classList.add("black");
+                document.querySelector(".tetris-field").append(div);
+            }
+        }
+    }
+}
+
+button.addEventListener("click", renderItems(tetrisArr));
+
+document.addEventListener("keydown", function (event) {
+    if (event.code === "ArrowRight") {
+        pieceGoesRight(tetrisArr);
+        renderItems(tetrisArr);
+    } else if (event.code === "ArrowLeft") {
+        pieceGoesLeft(tetrisArr);
+        renderItems(tetrisArr);
+    } else if (event.code === "ArrowDown") {
+        pieceGoesDown(tetrisArr);
+        renderItems(tetrisArr);
+    }
+})
+
 
 const iBlock1 = [[1, 1, 1, 1]];
 
@@ -151,51 +183,95 @@ const jBlockRotations = {
 };
 
 const lBlock1 = [
-    [0, 0, 0],
+    [0, 0, 1],
+    [1, 1, 1]
+];
+
+const lBlock2 = [
+    [1, 0],
+    [1, 0],
+    [1, 1]
+];
+
+const lBlock3 = [
     [1, 1, 1],
     [1, 0, 0]
 ];
 
-const lBlock2 = [
-    [0, 1, 0],
-    [0, 1, 0],
-    [0, 1, 1]
+const lBlock4 = [
+    [1, 1],
+    [0, 1],
+    [0, 1]
 ];
 
+const oBlock1 = [
+    [1, 1],
+    [1, 1]
+]
+
 const sBlock1 = [
-    [0, 0, 0],
     [0, 1, 1],
     [1, 1, 0]
 ];
 
 const sBlock2 = [
-    [1, 0, 0],
-    [1, 1, 0],
-    [0, 1, 0]
+    [1, 0],
+    [1, 1],
+    [0, 1]
+];
+
+
+const tBlock1 = [
+    [0, 1, 0],
+    [1, 1, 1],
+];
+
+const tBlock2 = [
+    [1, 0],
+    [1, 1],
+    [1, 0]
 ];
 
 const zBlock1 = [
-    [0, 0, 0],
     [1, 1, 0],
     [0, 1, 1]
 ];
 
 const zBlock2 = [
-    [0, 1, 0],
-    [1, 1, 0],
-    [1, 0, 0]
+    [0, 1],
+    [1, 1],
+    [1, 0]
 ];
 
-const tBlock1 = [
-    [0, 1, 0],
-    [1, 1, 1],
-    [0, 0, 0]
-];
+function randomNumber() {
+    const num = Math.floor(Math.random() * 7);
+    return num;
+}
 
-const tBlock2 = [
-    [0, 1, 0],
-    [0, 1, 0],
-    [0, 1, 1]
-];
+const allFirstBlocks = [iBlock1, jBlock1, lBlock1, oBlock1, sBlock1, tBlock1, zBlock1];
 
-const topLine = [-1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1];
+function chooseRandomBlock() {
+    const randomBlock = allFirstBlocks[Math.floor(Math.random() * 7)];
+    for (let i = 0; i < randomBlock.length; i++) {
+        for (let j = 0; j < randomBlock[0].length; j++) {
+            tetrisArr[i][j + Math.round((10 - randomBlock[0].length) / 2)] = randomBlock[i][j]
+        }
+    }
+}
+
+function isChoosenRancomBlockImported() {
+    for (let i = 0; i < 20; i++) {
+        for (let j = 0; j < 10; j++) {
+            if (tetrisArr[i][j] === 1) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+function importBlock() {
+    if (isChoosenRancomBlockImported) {
+        chooseRandomBlock();
+    }
+}
